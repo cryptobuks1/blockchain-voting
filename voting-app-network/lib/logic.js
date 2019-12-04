a02abea679e59d8a0e2881222c55b8ac50a37d6a
Aphrx <amalnnath20@yahoo.ca>
2019-12-03 20:53:03 -0800

    Add files via upload

--- voting-app-network/lib/logic.js
+++ voting-app-network/lib/logic.js
@@ -4,20 +4,22 @@
  */
 
 async function vote(vote) {
+    //if voter has not voted yet
     if (!vote.ifVotedAsset.votedOrNot) {
+	    //increase the total count of the political party being voted for by 1
         vote.candidateVoteAsset.totalVote = vote.candidateVoteAsset.totalVote + 1;
         return getAssetRegistry('org.byke.voting.candidateVote')
             .then(function (assetRegistry) {
-                return assetRegistry.update(vote.candidateVoteAsset);
+                return assetRegistry.update(vote.candidateVoteAsset); //update the network registry
             })
             .then(function () {
                 return getAssetRegistry('org.byke.voting.ifVoted')
                     .then(function (assetRegistry) {
-                        vote.ifVotedAsset.votedOrNot = true;
-                        return assetRegistry.update(vote.ifVotedAsset);
+                        vote.ifVotedAsset.votedOrNot = true; //set the vote boolean to true so the voter cannot vote again
+                        return assetRegistry.update(vote.ifVotedAsset); //update the netowrk registry with the new value
                     })
             });
     } else {
-        throw new Error('Vote already submitted!');
+        throw new Error('Vote already submitted!'); //give error if voter tries to vote again if they already voted
     }
 }
